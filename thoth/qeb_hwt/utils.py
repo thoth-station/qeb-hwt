9083dd1a5d060ebf96f0c02a4e006519d59e23f6
Francesco Murdaca <fmurdaca@redhat.com>
2020-06-16 10:24:07 +0200

    Remove empty part of reports
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/qeb_hwt/utils.py
+++ thoth/qeb_hwt/utils.py
@@ -38,113 +38,152 @@ def create_pretty_report_from_json(report: dict, is_justification: bool = False)
     final_df = pd.DataFrame(columns=["message", "type"])
 
     counter = 0
-    for product in products:
-        if "justification" in product.keys():
-            justifications = product["justification"]
-            if justifications:
-                for justification in justifications:
-                    final_df.loc[counter] = pd.DataFrame([justification]).iloc[0]
-                    counter += 1
+    # Consider the first product
+    product = products[0]
+
+    if "justification" in product.keys():
+        justifications = product["justification"]
+
+        if justifications:
+
+            for justification in justifications:
+                final_df.loc[counter] = pd.DataFrame([justification]).iloc[0]
+                counter += 1
 
     md += "\n\n" + final_df.to_markdown()
 
     if is_justification:
         return md
 
-    md += "\n\n" + "Packages in Advised Pipfile"
+    # Packages in Advised Pipfile
+    md += _add_packages_in_advised_pipfile_to_md_report(product=product, is_dev=False)
 
-    packages_names = []
-    packages_versions = []
+    # Dev-Packages in Advised Pipfile
+    md += _add_packages_in_advised_pipfile_to_md_report(product=product, is_dev=True)
 
-    for package_name, requested_version in report["products"][0]['project']["requirements"]["packages"].items():
-        packages_names.append(package_name)
-        packages_versions.append(requested_version)
+    requirements = product["project"]["requirements"]
 
-    data = {
-        "package_name": packages_names,
-        "pakage_version": packages_versions,
-    }
+    if "requires" in requirements:
+        if requirements["requires"]:
+            md += "\n\n" + "Requires in Advised Pipfile"
+            df = pd.DataFrame([requirements["requires"]])
+            md += "\n\n" + df.to_markdown()
 
-    df = pd.DataFrame(data)
-    md += "\n\n" + df.to_markdown()
+    if "source" in requirements:
+        if requirements["source"]:
+            md += "\n\n" + "Source in Advised Pipfile"
+            df = pd.DataFrame(requirements["source"])
+            md += "\n\n" + df.to_markdown()
 
-    md += "\n\n" + "Dev-Packages in Advised Pipfile"
+    # Packages in Advised Pipfile.lock
+    md += _add_packages_in_advised_pipfile_lock_to_md_report(product=product, is_dev=False)
 
-    packages_names = []
-    packages_versions = []
+    # Dev-Packages in Advised Pipfile.lock
+    md += _add_packages_in_advised_pipfile_lock_to_md_report(product=product, is_dev=True)
 
-    for package_name, requested_version in report["products"][0]['project']["requirements"]["dev-packages"].items():
-        packages_names.append(package_name)
-        packages_versions.append(requested_version)
+    # Runtime Environment
+    md += _add_runtime_environment_to_md_report(product=product)
 
-    data = {
-        "package_name": packages_names,
-        "package_version": packages_versions,
-    }
-
-    df = pd.DataFrame(data)
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Requires in Advised Pipfile"
-    df = pd.DataFrame([report["products"][0]['project']['requirements']['requires']])
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Source in Advised Pipfile"
-    df = pd.DataFrame(report["products"][0]['project']['requirements']['source'])
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Packages in Advised Pipfile.lock"
-    packages_names = []
-    packages_versions = []
-    packages_indexes = []
-
-    for package_name, data in report["products"][0]['project']["requirements_locked"]["default"].items():
-        packages_names.append(package_name)
-        packages_versions.append(data["version"])
-        packages_indexes.append(data["index"])
-
-    data = {
-        "package_name": packages_names,
-        "package_version": packages_versions,
-        "index": packages_indexes,
-    }
-    df = pd.DataFrame(data)
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Dev-Packages in Advised Pipfile.lock"
-    packages_names = []
-    packages_versions = []
-    packages_indexes = []
-
-    for package_name, data in report["products"][0]['project']["requirements_locked"]["develop"].items():
-        packages_names.append(package_name)
-        packages_versions.append(data["version"])
-        packages_indexes.append(data["index"])
-
-    data = {
-        "package_name": packages_names,
-        "package_version": packages_versions,
-        "index": packages_indexes,
-    }
-    df = pd.DataFrame(data)
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Runtime Environment"
-
-    md += "\n\n" + "Runtime Environment - Name"
-    df = pd.DataFrame([{"name": report["products"][0]['project']['runtime_environment']['name']}])
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Runtime Environment - CUDA"
-    df = pd.DataFrame([{"cuda_version": report["products"][0]['project']['runtime_environment']['cuda_version']}])
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Runtime Environment - Hardware"
-    df = pd.DataFrame([report["products"][0]['project']['runtime_environment']['hardware']])
-    md += "\n\n" + df.to_markdown()
-
-    md += "\n\n" + "Software Stack Score"
-    df = pd.DataFrame([{"score": report["products"][0]['score']}])
-    md += "\n\n" + df.to_markdown()
+    if "score" in product:
+        if product["score"]:
+            md += "\n\n" + "Software Stack Score"
+            df = pd.DataFrame([{"score": product["score"]}])
+            md += "\n\n" + df.to_markdown()
 
     return md
+
+
+def _add_packages_in_advised_pipfile_to_md_report(product: dict, is_dev: bool):
+    """Add Packages in Advised Pipfile to final report."""
+    md_report = ""
+
+    if is_dev:
+        packages = "dev-packages"
+    else:
+        packages = "packages"
+
+    if not product["project"]["requirements"][packages]:
+        return md_report
+
+    if is_dev:
+        md_report += "\n\n" + "Dev-Packages in Advised Pipfile"
+    else:
+        md_report += "\n\n" + "Packages in Advised Pipfile"
+
+    packages_names = []
+    packages_versions = []
+
+    for package_name, requested_version in product["project"]["requirements"][packages].items():
+        packages_names.append(package_name)
+        packages_versions.append(requested_version)
+
+    data = {"package_name": packages_names, "pakage_version": packages_versions}
+
+    df = pd.DataFrame(data)
+    md_report += "\n\n" + df.to_markdown()
+
+    return md_report
+
+
+def _add_packages_in_advised_pipfile_lock_to_md_report(product: dict, is_dev: bool):
+    """Add Packages in Advised Pipfile.lock to final report."""
+    md_report = ""
+
+    if is_dev:
+        packages = "develop"
+    else:
+        packages = "default"
+
+    if not product["project"]["requirements_locked"][packages]:
+        return md_report
+
+    if is_dev:
+        md_report += "\n\n" + "Dev-Packages in Advised Pipfile.lock"
+    else:
+        md_report += "\n\n" + "Packages in Advised Pipfile.lock"
+
+        packages_names = []
+        packages_versions = []
+        packages_indexes = []
+
+        for package_name, data in product["project"]["requirements_locked"][packages].items():
+            packages_names.append(package_name)
+            packages_versions.append(data["version"])
+            packages_indexes.append(data["index"])
+
+        data = {"package_name": packages_names, "package_version": packages_versions, "index": packages_indexes}
+        df = pd.DataFrame(data)
+        md_report += "\n\n" + df.to_markdown()
+
+    return md_report
+
+
+def _add_runtime_environment_to_md_report(product: dict):
+    """Add Packages in Advised Pipfile.lock to final report."""
+    md_report = ""
+
+    if not product["project"]["runtime_environment"]:
+        return md_report
+
+    runtime_environment = product["project"]["runtime_environment"]
+    md_report += "\n\n" + "Runtime Environment"
+
+    if "name" in runtime_environment:
+        if runtime_environment["name"]:
+            md_report += "\n\n" + "Runtime Environment - Name"
+            df = pd.DataFrame([{"name": runtime_environment["name"]}])
+            md_report += "\n\n" + df.to_markdown()
+
+    if "cuda_version" in runtime_environment:
+        if runtime_environment["cuda_version"]:
+            md_report += "\n\n" + "Runtime Environment - CUDA"
+            df = pd.DataFrame([{"cuda_version": runtime_environment["cuda_version"]}])
+            md_report += "\n\n" + df.to_markdown()
+
+    if "hardware" in runtime_environment:
+        if [h for h in runtime_environment["hardware"].keys() if runtime_environment["hardware"][h]]:
+            md_report += "\n\n" + "Runtime Environment - Hardware"
+            df = pd.DataFrame([runtime_environment["hardware"]])
+            md_report += "\n\n" + df.to_markdown()
+
+    return md_report
