0367f02d0a1ce8b697fb4e9c661011de896fc383
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-22 17:28:14 +0200

    Adjust imports (#75)
    * remove dependencies
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Adjust import
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ from octomachinery.utils.versiontools import get_version_from_scm_tag
 
 from thoth.common import init_logging
 from thoth.common import WorkflowManager
-from thoth.report_processing.adviser import Adviser
+from thoth.report_processing.components.adviser import Adviser
 
 from thoth.qeb_hwt.version import __version__ as qeb_hwt_version
 
